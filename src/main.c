732d2bb309c61adb1996b0a29c2b7a995db1697d
FourthRealm <160537701+FourthRealm@users.noreply.github.com>
2025-12-03 16:16:25 +1100

    Restarted

--- src/main.c
+++ src/main.c
@@ -1,29 +1,5 @@
 #include "raylib.h"
-#include "GameEngine.h"
-#include "Entity.h"
-#include "RenderComponent.h"
-#include <unistd.h>
-#include <libgen.h>
-#include <limits.h>
 
 int main(void) {
-    char exePath[PATH_MAX];
-    uint32_t size = sizeof(exePath);
-    if(_NSGetExecutablePath(exePath, &size) == 0) {
-        chdir(dirname(exePath));
-    }
-
-    InitialiseEngine();
-
-    EntityId id = CreateEntity((Vector2){100, 100}, (Vector2){200, 200});
-    AddRenderComponent(id, LoadTexture("assets/TestTiles.png"), WHITE);
-
-    while(IsGameRunning()) {
-        UpdateEngine();
-    }
-
-    ShutdownEngine();
-    CloseWindow();
-
     return 0;
 }
